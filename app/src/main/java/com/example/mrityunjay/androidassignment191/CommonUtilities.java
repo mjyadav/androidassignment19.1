999dfd09b9ec8801a3a819f0511b705c21cd96ef
mjyadav <mrityunjay.yadav0007@gmail.com>
2017-06-19 23:03:57 +0530

    ass
    ass

---
+++ app/src/main/java/com/example/mrityunjay/androidassignment191/CommonUtilities.java
@@ -0,0 +1,12 @@
+package com.example.mrityunjay.androidassignment191;
+
+/**
+ * Created by Mrityunjay on 19-06-2017.
+ */
+
+public class CommonUtilities {
+    public enum SERVICE_TYPE {
+        GET_DATA, ERROR_REPORT
+
+    }
+}
---
+++ app/src/main/java/com/example/mrityunjay/androidassignment191/OnWebServiceResult.java
@@ -0,0 +1,10 @@
+package com.example.mrityunjay.androidassignment191;
+
+/**
+ * Created by Mrityunjay on 19-06-2017.
+ */
+
+interface OnWebServiceResult {
+    public void getWebResponse(String result, CommonUtilities.SERVICE_TYPE type);
+}
+
